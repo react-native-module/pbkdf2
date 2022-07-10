4d96693e9c3e4c489c005eb6b3f6cb5f67950448
sacru2red <sacru2red@gmail.com>
2022-07-11 00:02:34 +0900

    add some digest support Like cryptojs, and works

--- android/src/main/java/com/reactnativepbkdf2/Pbkdf2Module.java
+++ android/src/main/java/com/reactnativepbkdf2/Pbkdf2Module.java
@@ -1,7 +1,6 @@
 package com.reactnativepbkdf2;
 
 import android.support.annotation.NonNull;
-import android.util.Base64;
 
 import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
@@ -12,8 +11,14 @@ import org.spongycastle.crypto.Digest;
 import org.spongycastle.crypto.digests.SHA1Digest;
 import org.spongycastle.crypto.digests.SHA256Digest;
 import org.spongycastle.crypto.digests.SHA512Digest;
+import org.spongycastle.crypto.digests.SHA224Digest;
+import org.spongycastle.crypto.digests.SHA384Digest;
+import org.spongycastle.crypto.digests.RIPEMD160Digest;
 import org.spongycastle.crypto.generators.PKCS5S2ParametersGenerator;
 import org.spongycastle.crypto.params.KeyParameter;
+
+import java.math.BigInteger;
+import java.nio.charset.StandardCharsets;
 
 public class Pbkdf2Module extends ReactContextBaseJavaModule {
   public static final String NAME = "Pbkdf2";
@@ -28,19 +33,43 @@ public class Pbkdf2Module extends ReactContextBaseJavaModule {
     return NAME;
   }
 
-  @ReactMethod
-  public void derive(String password, String salt, int iterations, int keySize, String hash, Promise promise) {
-    byte[] decodedPassword = android.util.Base64.decode(password, Base64.DEFAULT);
-    byte[] decodedSalt = android.util.Base64.decode(salt, Base64.DEFAULT);
-    Digest digest = new SHA1Digest();
-    if (hash.equals("sha-256")) {
-      digest = new SHA256Digest();
-    } else if (hash.equals("sha-512")) {
-      digest = new SHA512Digest();
-    }
+  public static String pbkdf2Native(String password, String salt, int iterations, int keySize, String hash) {
+    byte[] decodedPassword = password.getBytes(StandardCharsets.UTF_8);
+    byte[] decodedSalt = salt.getBytes(StandardCharsets.UTF_8);
+
+    Digest digest = getDigestByName(hash);
     PKCS5S2ParametersGenerator gen = new PKCS5S2ParametersGenerator(digest);
     gen.init(decodedPassword, decodedSalt, iterations);
     byte[] key = ((KeyParameter) gen.generateDerivedParameters(keySize * 8)).getKey();
-    promise.resolve(Base64.encodeToString(key,Base64.DEFAULT));
+    return toHex(key);
+  }
+
+  private static String toHex(byte[] bytes) {
+    BigInteger bi = new BigInteger(1, bytes);
+    return String.format("%0" + (bytes.length << 1) + "x", bi);
+  }
+
+  private static Digest getDigestByName(String digestName) {
+    switch (digestName.toLowerCase()) {
+      case "sha1":
+        return new SHA1Digest();
+      case "sha256":
+        return new SHA256Digest();
+      case "sha512":
+        return new SHA512Digest();
+      case "sha224":
+        return new SHA224Digest();
+      case "sha384":
+        return new SHA384Digest();
+      case "ripemd160":
+        return new RIPEMD160Digest();
+      default:
+        throw new Error("Invalid digest");
+    }
+  }
+
+  @ReactMethod
+  public void derive(String password, String salt, int iterations, int keySize, String hash, Promise promise) {
+    promise.resolve(pbkdf2Native(password, salt, iterations, keySize, hash));
   }
 }
