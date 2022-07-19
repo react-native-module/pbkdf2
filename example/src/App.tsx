360735bd4a7c71da0b608edc66397c41b835bff3
sacru2red <sacru2red@gmail.com>
2022-07-19 09:00:58 +0900

    chore: add Buffer result

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,10 +1,11 @@
 import * as React from 'react';
-
 import { StyleSheet, View, Text } from 'react-native';
 import { pbkdf2 } from '@react-native-module/pbkdf2';
+import { Buffer } from 'buffer';
 
 export default function App() {
   const [result, setResult] = React.useState<string | undefined>();
+  const [bufferResult, setBufferResult] = React.useState<string | undefined>();
 
   React.useEffect(() => {
     pbkdf2('cGFzc3dvcmQ=', 'c2FsdA==', 1, 16, 'sha256', (err, derivedKey) => {
@@ -14,11 +15,26 @@ export default function App() {
         setResult(derivedKey.toString());
       }
     });
+    pbkdf2(
+      Buffer.from('cGFzc3dvcmQ=', 'base64'),
+      Buffer.from('c2FsdA==', 'base64'),
+      1,
+      16,
+      'sha256',
+      (err, derivedKey) => {
+        if (err) {
+          console.warn(err.message);
+        } else {
+          setBufferResult(derivedKey.toString());
+        }
+      }
+    );
   }, []);
 
   return (
     <View style={styles.container}>
       <Text>Result: {result}</Text>
+      <Text>Buffer Result: {bufferResult}</Text>
     </View>
   );
 }
