8f15476e990096f94519da4664329be297f8bc80
sacru2red <sacru2red@gmail.com>
2022-08-04 14:05:25 +0900

    add comment

--- src/pbkdf2Browser/index.ts
+++ src/pbkdf2Browser/index.ts
@@ -1,5 +1,9 @@
 // https://github.com/crypto-browserify/pbkdf2
 // https://www.npmjs.com/package/pbkdf2
+
+// why make pbkdf2Browser directory
+// I have to resolve pbkdf2 -> @react-native-module/pbkdf2
+// prevent Circular Dependencies
 import { async as pbkdf2 } from './async';
 import { sync as pbkdf2Sync } from './sync';
 
